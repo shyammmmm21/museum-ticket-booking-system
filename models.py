b74e21c6ca22d309a0c67d7e640eb4fdf355faaf
shyammmmm21 <shyam.patel2025@gmail.com>
2025-03-28 05:03:23 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -6,6 +7,15 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    mobile = db.Column(db.String(15), unique=True, nullable=True)
+    
+     # Hash password before storing
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+
+    # Check if password is correct
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
